c189a260585ae39f4d28f181f3fe9867239c38c3
marozex <49114932+marozex@users.noreply.github.com>
2025-06-22 15:46:31 +0900

    サービス層追加

--- repositories/item_repository.go
+++ repositories/item_repository.go
@@ -10,10 +10,13 @@ type ItemMemoryRepository struct {
 	items []models.Item
 }
 
+// リポジトリ作成時には以下のようにして使う
+// repo := NewItemMemoryRepository(items)
 func NewItemMemoryRepository(items []models.Item) IItemRepository {
 	return &ItemMemoryRepository{items: items}
 }
 
+// ItemMemoryRepository型のすべてのインスタンスにFindAllメソッドを紐づけている
 func (r *ItemMemoryRepository) FindAll() (*[]models.Item, error) {
 	return &r.items, nil
 }
